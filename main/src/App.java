4d66a0c1e6de97459b8282ab497b67cf5af90fed
Ousmane Sow <nureynisow@gmail.com>
2019-01-13 17:50:56 +0000

    test variable assignements and print them

--- main/src/App.java
+++ main/src/App.java
@@ -1,5 +1,20 @@
 public class App{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        char $Letter = '$';
+        System.out.println($Letter);
+        String hello = "Hello World";
+        System.out.println(hello);
+        byte petit = 55; // set it to 555
+        System.out.println(petit);
+        int entier = 42;
+        System.out.println(entier);
+        long largeVal = 999999999999999L; // wont work for int (try to remove the L)
+        System.out.println(largeVal);
+        float decimal1 = 3.14F; // try to remove the F
+        System.out.println(decimal1);
+        double decimal2 = 3.14345678904567893456756789;
+        System.out.println(decimal2);
+        boolean trueOrFalse_ = true;
+        System.out.println(trueOrFalse_);
     }
 }
