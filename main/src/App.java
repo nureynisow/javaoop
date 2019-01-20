74915c43f3e5ff09283b45505c2650dd055d39f8
Ousmane Sow <nureynisow@gmail.com>
2019-01-20 18:44:16 +0000

    if statment

--- main/src/App.java
+++ main/src/App.java
@@ -1,20 +1,20 @@
+import java.io.IOException;
+ 
 public class App{
     public static void main(String[] args) {
-        char $Letter = '$';
-        System.out.println($Letter);
-        String hello = "Hello World";
-        System.out.println(hello);
-        byte petit = 55; // set it to 555
-        System.out.println(petit);
-        int entier = 42;
-        System.out.println(entier);
-        long largeVal = 999999999999999L; // wont work for int (try to remove the L)
-        System.out.println(largeVal);
-        float decimal1 = 3.14F; // try to remove the F
-        System.out.println(decimal1);
-        double decimal2 = 3.14345678904567893456756789;
-        System.out.println(decimal2);
-        boolean trueOrFalse_ = true;
-        System.out.println(trueOrFalse_);
+        System.out.println("Type a letter : ");
+        char c;        
+        try { 
+			c = (char) System.in.read();
+		} catch (IOException e) { 
+            return;
+		}
+        if(c == 'A' || c == 'a'){
+            System.out.println("you got an A");
+        } else if(c == 'B' || c == 'b'){
+            System.out.println("You got an B");
+        }else {
+            System.out.println("Play again with A or B");
+        }
     }
 }
